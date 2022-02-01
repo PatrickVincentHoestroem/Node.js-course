5eb2117ca1f3c7535caffcbd895bf6297a6c4cc9
PatrickVincentHoestroem <patrick.vincent@live.dk>
2022-02-02 00:32:36 +0100

    made some homework.

---
+++ course_notes/01.introduction_day/type_coercion.js
@@ -0,0 +1,20 @@
+
+// Type coercion
+console.log(typeof("Your age is: " + age));  // Show what type the data we log is...
+console.log("Your age is: ", age);  // Also show type of the data, but use comma and data types are now colored...
+
+// REPL (read, evaluate, print, loop).
+// A prompt is a REPL? (using jshell (java), python prompt as examples).
+// To REPL on Node.js, simply within powershell type <node>.
+
+// Teacher's REPL in node
+//
+// >"age" + 2
+// 'age2'
+// > [] + {}
+// '[object Object]'
+// > [] + 2
+// '2'
+// > {} + 2
+// [object Object]2
+// 
---
+++ course_notes/01.introduction_day/variable.js
@@ -0,0 +1,6 @@
+const your_name = "Patrick";
+console.log(your_name);
+
+// Concatenation
+const age = 2;
+console.log("Your age is: " + age);
